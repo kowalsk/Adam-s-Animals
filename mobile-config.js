cfc93577b2375964149d77418f0a92596f1b5370
Pawel Kowalski <kowalski.p@gmail.com>
2016-01-04 23:05:40 +0000

    Adam's Animals v0.5 Final
    Final product, converted to APK and uploaded to Google Play Store. I
    have not got Apple Paid Developer account yet so I can’t upload it
    there.

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Adams Animals',
+  description: 'A few random pictures of animals with sounds for infants and small children.',
+  version: '1.0'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-wild-animals-ldpi-36x36.png',
+  'android_mdpi': 'resources/icons/icon-wild-animals-mdpi-48x48.png',
+  'android_hdpi': 'resources/icons/icon-wild-animals-hdpi-72x72.png',
+  'android_xhdpi': 'resources/icons/icon-wild-animals-xhdpi-96x96.png'
+});
